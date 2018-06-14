3c9ea91c0c880fe6c6915f5e1375ca256cb4d2a1
eliottolivieri <eliott.94@hotmail.com>
2018-06-14 18:42:04 -0400

    project complete

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,36 +1,124 @@
 
 
 var wins = 0;
+
+
 var losses = 0;
-var ranNumber;
-var crys1;
-var crys2;
-var crys3;
-var crys4;
-var totScore;
+
+
+var ranNumber = 0;
+
+
+crys1= Math.floor(Math.random()*12)+1;
+crys2= Math.floor(Math.random()*12)+1;
+crys3= Math.floor(Math.random()*12)+1;
+crys4= Math.floor(Math.random()*12)+1;
+
+var totScore = 0;
+
+
 
 function onStart(){
-    ranNumber= Math.floor(Math.random()*120)+19;
+    ranNumber= Math.floor(19+Math.random()*101);
     crys1= Math.floor(Math.random()*12)+1;
     crys2= Math.floor(Math.random()*12)+1;
     crys3= Math.floor(Math.random()*12)+1;
     crys4= Math.floor(Math.random()*12)+1;
     totScore = 0;
+    $("#crystalNum").text(totScore);
+    $("#RanNum").text(ranNumber);
+
 }
 
 
 $(document).ready(function(){
     $("#c1").click(function(){
+        
         totScore = totScore + crys1;
         $("#crystalNum").text(totScore);
-
-        $
+        if($("#RanNum").text === "0"){
+            onstart();
+            
+        } 
+        if(totScore === ranNumber){
+            wins++;
+            $("#w").text("wins: " + wins);
+            onStart();
+        }
+        
+        if(totScore > ranNumber){
+                losses++;
+                $("#l").text("losses: " + losses);
+                onStart();
+        }
+    
+         
     });
 });
 
 $(document).ready(function(){
     $("#c2").click(function(){
-        $(this).animate({ height: "200px" });
-        $
+        
+        totScore = totScore + crys2;
+        $("#crystalNum").text(totScore); 
+        if($("#RanNum").text === "0"){
+            onstart();
+            
+        } 
+        if(totScore === ranNumber){
+            wins++;
+            $("#w").text("wins: " + wins);
+            onStart();
+        }
+        if(totScore > ranNumber){
+            losses++;
+            $("#l").text("losses: " + losses);
+            onStart();
+        }
     });
 });
+
+$(document).ready(function(){
+    $("#c3").click(function(){
+        
+        totScore = totScore + crys3;
+        $("#crystalNum").text(totScore); 
+        if($("#RanNum").text === "0"){
+            onstart();
+           
+        } 
+        if(totScore === ranNumber){
+            wins++;
+            $("#w").text("wins: " + wins);
+            onStart();
+        }
+        if(totScore > ranNumber){
+            losses++;
+            $("#l").text("losses: " + losses);
+            onStart();
+        } 
+    });
+});
+
+$(document).ready(function(){
+    $("#c4").click(function(){
+        
+        totScore = totScore + crys4;
+        $("#crystalNum").text(totScore);
+        if($("#RanNum").text === "0"){
+            onstart();
+           
+        } 
+        if(totScore === ranNumber){
+            wins++;
+            $("#w").text("wins: " + wins);
+            onStart();
+        }
+        if(totScore > ranNumber){
+            losses++;
+            $("#l").text("losses: " + losses);
+            onStart();
+        }
+    });
+});
+
